78018d69d439d6b8439a9e5e27048e899e425d31
ayushsingh24 <ayushkrsingh2412@gmail.com>
2021-05-22 19:46:08 +0530

    image to url field

--- landslide/models.py
+++ landslide/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class images(models.Model):
-    moisture = models.URLField(max_length=50,blank=True)
-    slope = models.URLField(max_length=50,blank=True)
-    output = models.URLField(max_length=50,blank=True)
+    moisture = models.URLField(max_length=200,blank=True)
+    slope = models.URLField(max_length=200,blank=True)
+    output = models.URLField(max_length=200,blank=True)
